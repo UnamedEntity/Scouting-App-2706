2e35382bee979081c64364755a2da606156089c8
AnishaJ44 <AnishaJ44@users.noreply.github.com>
2026-04-08 23:48:41 +0000

    deploy: ca077217c9b4f16d7ab4ee92f08a33bccb708617

--- service-worker.js
+++ service-worker.js
@@ -15,10 +15,10 @@ self.addEventListener('fetch', event => {
     caches.match(event.request).then(response => {
       return response || fetch(event.request).then(fetchResponse => {
         return caches.open(CACHE_NAME).then(cache => {
-          cache.put(event.request, fetchResponse.clone());
+          cache.put(event.request, fetchResponse.clone())
           return fetchResponse;
         });
-      });
+      })
     }).catch(() => caches.match('/'))
-  );
+  )
 });
